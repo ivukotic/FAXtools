e61086a5d46bf53f57c2441d6d5abefc7a0a39a3
Ilija Vukotic <ivukotic@uchicago.edu>
2013-04-12 14:48:13 -0500

    first real delays

--- FAXdelays/FAX_delays_tests.py
+++ FAXdelays/FAX_delays_tests.py
@@ -1,35 +1,12 @@
 #!/usr/bin/env python
-import subprocess, threading, os, sys, cx_Oracle,time
-import stomp, logging, datetime, ConfigParser
-
-logging.basicConfig()
-
-config = ConfigParser.ConfigParser()
-config.read("neet.cfg")
-HOST = config.get("Connection", "HOST")
-PORT = int(config.get("Connection", "PORT"))
-QUEUE = config.get("Connection", "QUEUE")
-
-
-def send (message):
-    """ Send message by stomp protocols.
-    @param message: the message being sent
-    
-    """  
-    conn = stomp.Connection([(HOST,PORT)])
-    conn.start()
-    conn.connect()
-    
-    conn.send(message,destination=QUEUE, ack='auto')
-    
-    try:
-       conn.disconnect()
-    except Exception:
-        'Exception on disconnect'
-
+import subprocess, threading, os, sys,time
+import logging, datetime
 
 timeouts=300
 sleeps=250
+
+DTS='//atlas/dq2/user/flegger/XXX/user.flegger.XXX.data12_8TeV.00212172.physics_Muons.merge.NTUP_SMWZ.f479_m1228_p1067_p1141_tid01007411_00'
+DTSFN='/NTUP_SMWZ.01007411._000062.XXX.root.1'
 
 sites=[]; # each site contains [name, host, redirector]
 
@@ -63,6 +40,8 @@ class site:
        s=s|(self.upstream<<1)
        s=s|(self.direct<<0)
        return s
+
+
 
 print 'Geting site list from AGIS...' 
 
@@ -107,22 +86,24 @@ class Command(object):
     
 
 
+
+
 for s in sites: s.prnt(-1) # print all
 print 'creating scripts to execute'
     
 print "================================= CHECK I =================================================="
     
-with open('checkDirect.sh', 'w') as f: # first check that site itself gives it's own file
+with open('checkDelays.sh', 'w') as f: # first check that site itself gives it's own file
     for s in sites:
-        logfile=s.name+'_to_'+s.name+'.log'
-        lookingFor = 'user.HironoriIto.xrootd.'+s.name+'/user.HironoriIto.xrootd.'+s.name+'-1M'
-        s.comm1='xrdcp -f -np -d 1 '+s.host+'//atlas/dq2/user/HironoriIto/'+lookingFor+' /dev/null >& '+logfile+' & \n'
+        logfile='delaysTo_'+s.name+'.log'
+        lookingFor = (DTS+DTSFN).replace('XXX',s.name)
+        s.comm1='xrdcp -f -np -d 1 root//glrd.usatlas.org'+lookingFor+' /dev/null >& '+logfile+' & \n'
         f.write(s.comm1)
     f.close()
 
 #sys.exit(0)
 print 'executing all of the xrdcps in parallel. 5 min timeout.'
-com = Command("source checkDirect.sh")    
+com = Command("source checkDelays.sh")    
 com.run(timeouts)
 time.sleep(sleeps)
 
@@ -131,7 +112,7 @@ print 'checking log files'
 
 # checking which sites gave their own file directly
 for s in sites:  # this is file to be asked for
-    logfile=s.name+'_to_'+s.name+'.log'
+    logfile='delaysTo_'+s.name+'.log'
     with open(logfile, 'r') as f:
         lines=f.readlines()
         succ=False
@@ -150,122 +131,7 @@ for s in sites: s.prnt(0)  #print only real sites
 
 #sys.exit(0)
 
-print "================================= CHECK II ================================================="
-
-with open('checkUpstream.sh', 'w') as f: # ask good sites for unexisting file
-    for s in sites:
-        if s.direct==0: continue
-        logfile='upstreamFrom_'+s.name+'.log'
-        lookingFor = 'user.HironoriIto.xrootd.'+s.name+'/user.HironoriIto.xrootd.unexisting-1M'
-        comm='xrdcp -f -np -d 1 '+s.host+'//atlas/dq2/user/HironoriIto/'+lookingFor+' /dev/null >& '+logfile+' & \n'
-        f.write(comm)            
-    f.close()
-    
-print 'executing all of the redirection xrdcps in parallel. 5 min timeout.'
-com = Command("source checkUpstream.sh")    
-com.run(timeouts)
-time.sleep(sleeps)
-
-
-for s in sites:
-    if s.direct==0: continue
-    logfile='upstreamFrom_'+s.name+'.log'
-    with open(logfile, 'r') as f:
-        print logfile
-        lines=f.readlines()        
-        reds=[]
-        for l in lines:
-            if l.count("Received redirection")>0:
-                red=l[l.find("[")+1 : l.find("]")]
-                reds.append(red.split(':')[0])
-        print 'redirections:',reds
-        if s.redirector.split(':')[0]  in reds:
-            s.upstream=1
-            print 'redirection works'
-        else:    
-            s.upstream=0
-            print 'redirection does not work'
-
-#sys.exit(0)
-print "================================= CHECK III ================================================"
-
-with open('checkDownstream.sh', 'w') as f: # ask global redirectors for files belonging to good sites
-    for s in sites:
-        if s.direct==0: continue
-        logfile='downstreamTo_'+s.name+'.log'
-        lookingFor = 'user.HironoriIto.xrootd.'+s.name+'/user.HironoriIto.xrootd.'+s.name+'-1M'
-        comm='xrdcp -f -np -d 1 root://'+s.redirector+'//atlas/dq2/user/HironoriIto/'+lookingFor+' /dev/null >& '+logfile+' & \n'
-        f.write(comm)            
-    f.close()
-
-print 'executing all of the redirection xrdcps in parallel. 5 min timeout.'
-com = Command("source checkDownstream.sh")    
-com.run(timeouts)
-time.sleep(sleeps)
-
-for s in sites:
-    if s.direct==0: continue
-    logfile='downstreamTo_'+s.name+'.log'
-    with open(logfile, 'r') as f:
-        print 'Checking file: ', logfile
-        lines=f.readlines()
-        succ=False
-        reds=[]
-        for l in lines:
-            if l.startswith(" BytesSubmitted"):
-                succ=True
-                s.downstream=1
-        if succ==False: 
-            print 'Did not work.'
-            s.downstream=0 
-            continue                
-        print 'OK'
-                
-print "================================= CHECK IV ================================================"
-
-with open('checkSecurity.sh', 'w') as f: # deletes proxy and then tries to directly access the files
-    f.write('rm -f /tmp/x509* \n')
-    for s in sites:
-        if s.direct==0: continue
-        logfile='checkSecurity_'+s.name+'.log'
-        lookingFor = 'user.HironoriIto.xrootd.'+s.name+'/user.HironoriIto.xrootd.'+s.name+'-1M'
-        s.comm1='xrdcp -f -np -d 1 '+s.host+'//atlas/dq2/user/HironoriIto/'+lookingFor+' /dev/null >& '+logfile+' & \n'
-        f.write(s.comm1)
-    f.close()
-
-#sys.exit(0)
-print 'executing all of the xrdcps in parallel. 5 min timeout.'
-com = Command("source checkSecurity.sh")
-com.run(timeouts)
-time.sleep(sleeps)
-
-
 print 'checking log files'
-
-# checking which sites gave their own file directly
-for s in sites:  # this is file to be asked for
-    if s.direct==0: continue
-    logfile='checkSecurity_'+s.name+'.log'
-    with open(logfile, 'r') as f:
-        lines=f.readlines()
-        succ=False
-        for l in lines:
-            # print l
-            if l.startswith(" BytesSubmitted"):
-                succ=True
-                break
-        if succ==True:
-            print logfile, "security does not work."
-            s.security=0
-        else:
-            s.security=1
-            print logfile, "security works."
 
 for s in sites: s.prnt(0)  #print only real sites
 
-
-print '--------------------------------- Uploading results ---------------------------------'
-ts=datetime.datetime.now()
-ts=ts.replace(microsecond=0)
-for s in sites:
-    send ('siteName: '+ s.name + '\nmetricName: FAXprobe1\nmetricStatus: '+str(s.status())+'\ntimestamp: '+ts.isoformat(' ')+'\n')      
