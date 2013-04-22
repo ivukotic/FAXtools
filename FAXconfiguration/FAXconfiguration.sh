147c9db4ce5d19f9e872c521c03ca5bc392cf736
Ilija Vukotic <ivukotic@uchicago.edu>
2013-04-22 22:20:20 +0200

    change to 17.7.0

--- FAXconfiguration/FAXconfiguration.sh
+++ FAXconfiguration/FAXconfiguration.sh
@@ -13,7 +13,7 @@ rm check*.sh *.log
 
 source /afs/cern.ch/project/gd/LCG-share/current/etc/profile.d/grid_env.sh; 
 
-source $AtlasSetup/scripts/asetup.sh 17.6.0,noTest
+source $AtlasSetup/scripts/asetup.sh 17.7.0,noTest
 
 voms-proxy-init -cert /afs/cern.ch/user/i/ivukotic/.globus/usercert.pem -key /afs/cern.ch/user/i/ivukotic/.globus/userkey.pem -voms atlas -pwstdin < /afs/cern.ch/user/i/ivukotic/gridlozinka.txt
 
