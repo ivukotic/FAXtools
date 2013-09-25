091c4ac9792e518ec3470412f8fa81582b30d5f1
Ilija Vukotic <ivukotic@uchicago.edu>
2013-09-25 23:41:22 +0200

    dbg

--- FAXconfiguration/FAX_configuration_tests.py
+++ FAXconfiguration/FAX_configuration_tests.py
@@ -102,7 +102,7 @@ try:
     req = urllib2.Request("http://atlas-agis-api-0.cern.ch/request/service/query/get_se_services/?json&state=ACTIVE&flavour=XROOTD", None)
     opener = urllib2.build_opener()
     f = opener.open(req)
-    res=simplejson.load(f)
+    res=json.load(f)
     for s in res:
         print  s["name"],s["rc_site"], s["endpoint"], s["redirector"]["endpoint"]
         si=site(s["name"].lower(),s["rc_site"].lower(), s["endpoint"], s["redirector"]["endpoint"])
@@ -121,7 +121,7 @@ try:
     req = urllib2.Request("http://atlas-agis-api.cern.ch/request/service/query/get_redirector_services/?json&state=ACTIVE", None)
     opener = urllib2.build_opener()
     f = opener.open(req)
-    res=simplejson.load(f)
+    res=json.load(f)
     for s in res:
         print s["name"], s["endpoint"]
         redirectors.append(redirector(s["name"],s["endpoint"]))
@@ -461,7 +461,7 @@ try:
     req = urllib2.Request(ur, None)
     opener = urllib2.build_opener()
     f = opener.open(req)
-    res=simplejson.load(f)
+    res=json.load(f)
     res=res["testDetails"]
     for m in res:
         #print m
