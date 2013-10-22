2cfb3d34ba35b5999eed3cc3d30b756344f1d39d
Ilija Vukotic <ivukotic@cern.ch>
2013-10-22 11:49:17 +0200

    parallel

--- FAXtestFDR/testUCT2-S6_pnfs.sh
+++ FAXtestFDR/testUCT2-S6_pnfs.sh
@@ -2,7 +2,7 @@ endpoint=root://fax.mwt2.org
 SITE=MWT2rucio
 R=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/x86_64/root/5.34.10-x86_64-slc5-gcc4.3/bin/root
 kom1="$R -q -b \"list.C(\\\""
-kom2="\\\")\" >> $SITE.log"
+kom2="\\\")\" >> $SITE.log &"
 
 eval $kom1$endpoint/pnfs/uchicago.edu/atlasdatadisk//rucio/user/flegger/17/56/NTUP_SMWZ.00987986._000010.MWT2.root.1$kom2
 eval $kom1$endpoint/pnfs/uchicago.edu/atlasdatadisk//rucio/user/flegger/14/7d/NTUP_SMWZ.00987986._000006.MWT2.root.1$kom2
@@ -35,6 +35,8 @@ eval $kom1$endpoint/pnfs/uchicago.edu/atlasdatadisk//rucio/user/flegger/73/c0/NT
 eval $kom1$endpoint/pnfs/uchicago.edu/atlasdatadisk//rucio/user/flegger/f5/6e/NTUP_SMWZ.00986520._000001.MWT2.root.1$kom2
 eval $kom1$endpoint/pnfs/uchicago.edu/atlasdatadisk//rucio/user/flegger/b5/e2/NTUP_SMWZ.00986520._000002.MWT2.root.1$kom2
 eval $kom1$endpoint/pnfs/uchicago.edu/atlasdatadisk//rucio/user/flegger/5d/3f/NTUP_SMWZ.01007411._000113.MWT2.root.1$kom2
+
+exit 0
 eval $kom1$endpoint/pnfs/uchicago.edu/atlasdatadisk//rucio/user/flegger/36/95/NTUP_SMWZ.01007411._000102.MWT2.root.2$kom2
 eval $kom1$endpoint/pnfs/uchicago.edu/atlasdatadisk//rucio/user/flegger/55/fc/NTUP_SMWZ.01007411._000121.MWT2.root.2$kom2
 eval $kom1$endpoint/pnfs/uchicago.edu/atlasdatadisk//rucio/user/flegger/d0/9d/NTUP_SMWZ.01007411._000048.MWT2.root.1$kom2
