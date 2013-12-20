973d423ff9ce043ce5727f4250ec549a26babb06
Ilija Vukotic <ivukotic@cern.ch>
2013-12-20 11:57:19 -0600

    change in FAX_AP_status

--- FAX_AP_status/FAX_AP_status.sh
+++ FAX_AP_status/FAX_AP_status.sh
@@ -11,19 +11,18 @@ WD=/afs/cern.ch/user/i/ivukotic/FAXtools/FAX_AP_status/
 cd $WD
 rm checkAP*.sh checkAP_*.log
 
-#source /afs/cern.ch/project/gd/LCG-share/current/etc/profile.d/grid_env.sh; 
-#source $AtlasSetup/scripts/asetup.sh 17.8.0,noTest
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
 
-localSetupGLite
-localSetupPython 2.6.5-x86_64-slc5-gcc43
+localSetupFAX
+#localSetupGLite
+#localSetupPython 2.6.5-x86_64-slc5-gcc43
 
 voms-proxy-init -cert /afs/cern.ch/user/i/ivukotic/.globus/usercert.pem -key /afs/cern.ch/user/i/ivukotic/.globus/userkey.pem -voms atlas -pwstdin < /afs/cern.ch/user/i/ivukotic/gridlozinka.txt
 
-asetup 17.8.0,noTest
+#asetup 17.8.0,noTest
 
-source /afs/cern.ch/project/xrootd/software/setup.sh test/3.3.3-rc1/x86_64-slc6-gcc45-opt
+#source /afs/cern.ch/project/xrootd/software/setup.sh test/3.3.3-rc1/x86_64-slc6-gcc45-opt
 
 cd $WD
 python FAX_AP_status.py
