f77f5b106a68cbcfe997f8257e7e0be2f410824f
Ilija Vukotic <ivukotic@cern.ch>
2013-12-31 16:43:54 -0600

    small changes

--- FAXconfiguration/FAXconfiguration.sh
+++ FAXconfiguration/FAXconfiguration.sh
@@ -11,19 +11,19 @@ voms-proxy-init -cert /afs/cern.ch/user/i/ivukotic/.globus/usercert.pem -key /af
 echo "deleting old files..."
 
 WD=/afs/cern.ch/user/i/ivukotic/FAXtools/FAXconfiguration/
-
 cd $WD
-rm check*.sh *.log *.zip
+rm check*.sh 
+rm *.log 
+rm *.zip
 
-cd $WD
 python FAX_configuration_tests.py
 
 #copy to afs web space
 cp *.log /afs/cern.ch/user/i/ivukotic/www/logs/FAXconfiguration/.
 
 #upload to FAXBOX
-#da=$(date +"%Y-%m-%d %H:%M")
-#zip SSB_FAX_endpoints_logs-$da.zip *.log
+da=$(date +"%Y-%m-%dT%H%M")
+zip SSB_FAX_endpoints_logs-$da.zip *.log
 #xrdcp SSB_FAX_endpoints_logs-$da.zip root://faxbox.usatlas.org//user/ivukotic
 
 #copy to MWT2 web space
