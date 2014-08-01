ca59d8e07e899883c85479098c10100d9b50dad3
Ilija Vukotic <ivukotic@cern.ch>
2014-08-01 11:05:27 -0500

    added code to upload overflow data to GAE

---
+++ overflow/uploadOverflow.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import  os, sys, time, datetime
+import urllib2, urllib
+
+try: import simplejson as json
+except ImportError: import json
+
+headers={ 'Content-Type': 'application/json' }
+
+try:
+    req = urllib2.Request("http://bigpanda.cern.ch/jobs/?transfertype=fax&hours=1", None, headers)
+    opener = urllib2.build_opener()
+    f = opener.open(req,timeout=50)
+    res=json.load(f)
+    f.close()
+    cleaned=[]
+    for j in res:
+        if j['jobstatus']=='activated': continue
+        if j['jobstatus']=='running': continue
+        if j['jobstatus']=='holding': continue
+        print j['pandaid'],j['jeditaskid'], j['jobstatus'],j['currentpriority'], j['computingsite'],j['produsername'],j['creationtime'],j['waittime'],j['starttime'],j['duration'],j['cpuconsumptiontime']
+        jo=[j['pandaid'],j['jeditaskid'], j['jobstatus'],j['currentpriority'], j['computingsite'],j['produsername'],j['creationtime'],j['waittime'],j['starttime'],j['duration'],j['cpuconsumptiontime']]
+        cleaned.append(jo)
+    json_data = json.dumps(cleaned)
+
+except:
+    print "# Can't load from bigpandamon ", sys.exc_info()[0]
+
+print json_data
+
+try:
+    req = urllib2.Request("http://1-dot-waniotest.appspot.com/CopyOverflow",json_data,{ 'Content-Type': 'application/json' })
+    opener = urllib2.build_opener()
+    f = opener.open(req,timeout=50)
+    res=f.read()
+    print res
+    f.close()
+except:
+    print "# Can't upload to GAE", sys.exc_info()[0]
+
+
+
+# {"ddmerrordiag": "", "inputfiletype": "", "creationtime": "2014-07-30T08:53:07+00:00", "jobstatus": "finished", "vo": "atlas", "waittime": "0:15:17", "destinationse": "", "exeerrorcode": 0, "duration": "0:21:42", "brokerageerrorcode": 0, "cloud": "US", "workinggroup": "", "homepackage": "", "prodsourcelabel": "user", "ddmerrorcode": 0, "produsername": "Sarah Louise Williams", "taskbuffererrordiag": "", "jobdispatchererrorcode": 0, "attemptnr": 1, "superrorcode": 0, "homecloud": null, "taskid": 3, "transexitcode": "0", "jobinfo": "", "currentpriority": 962, "transformation": "runGen-00-00-02", "jobdispatchererrordiag": "", "pandaid": 2230748170, "piloterrorcode": 0, "priorityrange": "900:999", "superrordiag": "", "jobsetid": 108572, "computingelement": "ANALY_BNL_SHORT-condor", "processingtype": "panda-client-0.5.5-jedi-run", "taskbuffererrorcode": 0, "cpuconsumptiontime": 1140, "jobname": "user.williams.TestGrid290714.00206971/", "brokerageerrordiag": "", "atlasrelease": "", "endtime": "2014-07-30T09:30:06+00:00", "computingsite": "ANALY_BNL_SHORT", "exeerrordiag": "", "jobsetrange": "108500:108599", "errorinfo": "", "inputfileproject": "user", "jeditaskid": 4002606, "specialhandling": "", "modificationtime": "2014-07-30T09:50:15+00:00", "starttime": "2014-07-30T09:08:24+00:00", "piloterrordiag": ""}
--- tools/n2n.py
+++ tools/n2n.py
@@ -1,12 +1,12 @@
 import sys
 import hashlib
 m = hashlib.md5()
-print sys.argv[1]
+#print sys.argv[1]
 s=sys.argv[1].split(':')[0]
 s=s.replace('/','.')
 n=sys.argv[1].split(':')[1]
-print 'scope',s
-print 'name',n
+#print 'scope',s
+#print 'name',n
 m.update(s+":"+n)
 r=m.hexdigest()
 print "rucio/"+s.replace('.','/')+"/"+m.hexdigest()[0:2]+'/'+m.hexdigest()[2:4]+'/'+n
