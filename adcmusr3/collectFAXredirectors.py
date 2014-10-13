e399b5e4dac710b6cfaa30705ce7694a8d4f0097
ivukotic <ivukotic@cern.ch>
2014-10-13 21:58:22 +0200

    updating FAX endpoints and redirectors status scripts.

---
+++ adcmusr3/collectFAXredirectors.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+import sys
+import stomp
+import time
+import json
+import socket
+
+from agisconf import agis
+from datetime import datetime
+
+global messages
+messages=[]
+
+hostalias='dashb-test-mb.cern.ch'
+s=socket.gethostbyname_ex(hostalias)
+print 'aliases: ', s[2]
+allhosts=[]
+for a in s[2]:
+    allhosts.append([(a, 6162)])
+
+queue = '/queue/faxmon.redirectors'
+
+class MyListener(object):
+    def on_error(self, headers, message):
+        print 'received an error %s' % message
+    
+    def on_message(self, headers, message):
+        #print 'received a message %s' % message
+        messages.append(message)
+    
+
+sites=[]
+class site:
+    name=''
+    address=''
+    status=0
+    times=0
+    def logpostfix(self):
+        return '_'+self.times[:16].replace(' ','T').replace(':','')+'.log'
+
+for host in allhosts:
+# Connect to the stompserver, listen to the queue for 2 seconds, print the messages and disconnect
+    try:
+        conn = stomp.Connection(host, use_ssl=True, ssl_key_file='/etc/grid-security/hostkey.pem', ssl_cert_file='/etc/grid-security/hostcert.pem')
+        conn.set_listener('MyConsumer', MyListener())
+        conn.start()
+        conn.connect()
+        conn.subscribe(destination = queue, ack = 'auto', headers = {})    
+        time.sleep(2)  
+    finally:
+        conn.disconnect()
+        for message in messages:
+            s=site()
+            message=message.split('\n')
+            for l in message:
+                # print l
+                l=l.split(": ")
+                if len(l)<2: continue
+                l[1]=l[1].strip()
+                print l[0],l[1]
+                if l[0]=='redirectorName': s.name=l[1].upper()
+                if l[0]=='metricName': continue
+                if l[0]=='address': s.address=l[1]
+                if l[0]=='metricStatus': s.status=int(l[1])
+                if l[0]=='timestamp': s.times = l[1]
+            sites.append(s)        
+
+print 'writing red_address.json'
+
+#site=' http://athena-infoioperformance.web.cern.ch/athena-infoIOperformance/logs/FAXconfiguration/'
+site=' http://www.mwt2.org/ssb/'
+
+f1 = open('red_address.json','w')
+for s in sites:
+    js=s.times+' '+s.name+' '+s.address
+    if s.status&1: 
+        js+=' gray'
+    else:
+        js+=' white'
+    js+=site+' \n'
+    f1.write(js)
+f1.close()        
+
+print 'writing red_upstream.json'
+
+f2 = open('red_upstream.json','w')
+for s in sites:
+    js=s.times+' '+s.name
+    log='checkRedirectorUpstream_'+s.name
+    sta=' OK green'
+    if s.status&4: sta=' NoUpstreamRedirection red'
+    if s.status&1: sta=' offline gray'
+    js+=sta+site+log+s.logpostfix()+' \n'
+    f2.write(js)
+f2.close()
+
+print 'writing red_downstream.json'
+   
+f3 = open('red_downstream.json','w')
+for s in sites:
+    js=s.times+' '+s.name
+    log='checkRedirectorDownstream_'+s.name
+    sta=' OK green'
+    if s.status&2: sta=' NoDownstreamRedirection red'
+    if s.status&1: sta=' offline gray'
+    js+=sta+site+log+s.logpostfix()+' \n'
+    f3.write(js)
+f3.close()
+
+
+print 'writing red_mailing.json'
+
+content={}
+sits=agis.list_sites()
+for s in sites:
+    continue
+    if s.status==15 or s.status==999: continue
+    msg=''
+    if not s.status&(1<<0): msg+='Direct xrootd access not working. '
+    if not s.status&(1<<2): msg+='Data unreachable via parent redirector. '
+    if not s.status&(1<<1): msg+='No upstream failover.'
+    if not s.status&(1<<3): msg+='ATLAS role extension not enabled for access.'
+    print s.name, msg
+    for cloud,sit in sits.iteritems():
+        for si in sit:
+            if si.name.upper()!=s.name: continue
+            if not cloud in content:
+                content[cloud]=[]   
+            content[cloud].append({si.name:msg})
+
+f6 = open('red_mailing.json','w')
+f6.write(json.dumps(content))
+f6.close()
+
+print 'done.'
---
+++ adcmusr3/generateInstantMails.py
@@ -0,0 +1,236 @@
+#!/usr/bin/env python
+import os, sys, time
+from  datetime import datetime
+from datetime import timedelta
+try: import simplejson as json
+except ImportError: import json
+
+import urllib2
+
+hours=5
+cuthours=3
+limtime=timedelta(0,cuthours*3600)
+curtime=datetime.now()
+cuttime=curtime-timedelta(0,hours*3600)
+
+class site:
+        direct=0
+        directfails=timedelta(0)
+        directworks=timedelta(0)
+        upstream=0
+        upstreamfails=timedelta(0)
+        upstreamworks=timedelta(0)
+        downstream=0
+        downstreamfails=timedelta(0)
+        downstreamworks=timedelta(0)
+        monitoring=0
+        monitoringfails=timedelta(0)
+        monitoringworks=timedelta(0)
+        lastReported=[0,0,0,0] # direct, upstream,downstream, monitoring. 0 - good news, 1 - bad news.
+        def __init__(self,na,cl):
+                self.name=na
+		self.cloud=cl
+		self.lastReported=[0,0,0,0]
+        def prn(self):
+                print self.name, self.cloud
+                print  "directfails: ",self.directfails,"/",self.direct
+                print  "directworks: ",self.directworks
+		print  "upstream: ",self.upstreamfails,"/",self.upstream
+                print  "downstream: ",self.downstreamfails,"/",self.downstream
+		print  "last reported: ",self.lastReported
+        def mess(self):
+                ret=''
+                if self.directfails>limtime and self.direct==3:
+			self.directfails-=timedelta(0,0,self.directfails.microseconds)
+                        ret+="Failing to copy file from site during more than "+str(cuthours)+" in last "+str(hours)+" hours. "
+                if self.upstreamfails>limtime and self.upstream==3:
+			self.upstreamfails-=timedelta(0,0,self.upstreamfails.microseconds)
+                        ret+="Failing to redirect upstream from site during more than "+str(cuthours)+" in last "+str(hours)+" hours. "
+                if self.downstreamfails>limtime and self.downstream==3:
+			self.downstreamfails-=timedelta(0,0,self.downstreamfails.microseconds)
+                        ret+="Failing to respond to request from redirector during more than "+str(cuthours)+" in last "+str(hours)+" hours. "
+                return ret
+
+
+
+#direct
+url="http://dashb-atlas-ssb.cern.ch/dashboard/request.py/getplotdata?time=1&dateFrom=&dateTo=&sites=all&clouds=all&batch=1&columnid=10083"
+print url
+try:
+	response=urllib2.Request(url,None)
+	opener = urllib2.build_opener()
+	f = opener.open(response)
+	data = json.load(f)
+	data=data["csvdata"]
+except:
+    print "Unexpected error:", sys.exc_info()[0]
+
+Sites=dict()
+for si in data:
+	n=si['VOName']
+	if n not in Sites:
+		s=site(n,si['Cloud'])
+		Sites[n]=s
+	st=datetime.strptime(si["Time"],'%Y-%m-%dT%H:%M:%S')
+	et=datetime.strptime(si["EndTime"],'%Y-%m-%dT%H:%M:%S')
+	if et<cuttime: continue  # throws away too early measurements
+	if st<cuttime: st=cuttime # cuts to exact cutoff time 
+	if et>curtime: # current state
+		et=curtime
+		Sites[n].direct=si['COLOR']
+        if si['COLOR']==5: Sites[n].directworks+= (et - st)
+	if si['COLOR']==3: Sites[n].directfails+= (et - st)
+
+##upstream
+url="http://dashb-atlas-ssb.cern.ch/dashboard/request.py/getplotdata?time=1&dateFrom=&dateTo=&sites=all&clouds=all&batch=1&columnid=10084"
+print url
+try:
+        response=urllib2.Request(url,None)
+        opener = urllib2.build_opener()
+        f = opener.open(response)
+        data = json.load(f)
+        data=data["csvdata"]
+except:
+    print "Unexpected error:", sys.exc_info()[0]
+
+for si in data:
+        n=si['VOName']
+        if n not in Sites:
+                s=site(n,si['Cloud'])
+                Sites[n]=s
+        st=datetime.strptime(si["Time"],'%Y-%m-%dT%H:%M:%S')
+        et=datetime.strptime(si["EndTime"],'%Y-%m-%dT%H:%M:%S')
+        if et<cuttime: continue  # throws away too early measurements
+        if st<cuttime: st=cuttime # cuts to exact cutoff time
+        if et>curtime: # current state
+                et=curtime
+                Sites[n].upstream=si['COLOR']
+        if si['COLOR']==5: Sites[n].upstreamworks+= (et - st)
+        if si['COLOR']==3: Sites[n].upstreamfails+= (et - st)
+
+
+##downstream
+url="http://dashb-atlas-ssb.cern.ch/dashboard/request.py/getplotdata?time=1&dateFrom=&dateTo=&sites=all&clouds=all&batch=1&columnid=10085"
+print url
+try:
+        response=urllib2.Request(url,None)
+        opener = urllib2.build_opener()
+        f = opener.open(response)
+        data = json.load(f)
+        data=data["csvdata"]
+except:
+    print "Unexpected error:", sys.exc_info()[0]
+
+for si in data:
+        n=si['VOName']
+        if n not in Sites:
+                s=site(n,si['Cloud'])
+                Sites[n]=s
+        st=datetime.strptime(si["Time"],'%Y-%m-%dT%H:%M:%S')
+        et=datetime.strptime(si["EndTime"],'%Y-%m-%dT%H:%M:%S')
+        if et<cuttime: continue  # throws away too early measurements
+        if st<cuttime: st=cuttime # cuts to exact cutoff time
+        if et>curtime: # current state
+                et=curtime
+                Sites[n].downstream=si['COLOR']
+        if si['COLOR']==5: Sites[n].downstreamworks+= (et - st)
+        if si['COLOR']==3: Sites[n].downstreamfails+= (et - st)
+
+
+#monitoring
+
+
+# lastReported
+try:
+	f1 = open('LastReported.json','r')
+        prev=json.load(f1)
+        for c in prev:
+		#print c, prev[c]
+                if c in Sites:
+			Sites[c].lastReported=prev[c]
+        f1.close()
+except:
+	pass
+
+# exit immediately in case too many direct access problems.
+totFailedDirect=totFailedUp=totFailedDown=0
+for sn,s in Sites.items():
+	if s.direct==3: totFailedDirect+=1
+        if s.upstream==3: totFailedUp+=1
+        if s.downstream==3: totFailedDown+=1
+if totFailedDirect>25 or totFailedUp>25 or totFailedDown>25:
+	print "XXXXXXXXXXXXXXXX PROBLEM XXXXXXXXXXXXXXXXXX"
+	print "X    A LARGE NUMBER OF FAILED TESTS       X"
+        print "X    RECHECK MANUALY.                     X"
+        print "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+	sys.exit(1)
+
+
+alljson={}
+clouds=dict() # for bad news
+cleared=dict() # for cleared
+alljson["failing"]=clouds
+alljson["cleared"]=cleared
+for sn,s in Sites.items():
+	# if was previously red and now is green and it is green for > 3 hours pronounce cleared	
+	if s.lastReported[0]==1 and s.directworks>limtime and s.direct==5:
+		if s.cloud not in cleared:
+			cleared[s.cloud]=dict()
+		cleared[s.cloud][sn]="A direct access to files at site "+sn+" is now possible. "
+		print "CLEARED direct!",sn, s.lastReported
+		s.lastReported[0]=0
+
+        if s.lastReported[1]==1 and s.upstreamworks>limtime and s.upstream==5:
+                if s.cloud not in cleared:
+                        cleared[s.cloud]=dict()
+                if sn not in cleared[s.cloud]: cleared[s.cloud][sn]=''
+		cleared[s.cloud][sn]+="An upstream redirection from site "+sn+" is now possible. "
+                print "CLEARED upstream!",sn, s.lastReported
+                s.lastReported[1]=0	
+
+        if s.lastReported[2]==1 and s.downstreamworks>limtime and s.downstream==5:
+                if s.cloud not in cleared:
+                        cleared[s.cloud]=dict()
+                if sn not in cleared[s.cloud]: cleared[s.cloud][sn]=''
+                cleared[s.cloud][sn]+="A downstream redirection to site "+sn+" is now possible. "
+                print "CLEARED downstream!",sn, s.lastReported
+                s.lastReported[2]=0
+
+	# if was previously green and now is red and it has been red for more than 3 hours pronounce blacklisted
+	if s.lastReported[0]==0 and s.directfails>limtime and s.direct==3: 
+		if s.cloud not in clouds:
+			clouds[s.cloud]=dict()
+		clouds[s.cloud][sn]="Failing to directly copy file from site during more than "+str(cuthours)+" in last "+str(hours)+" hours. "
+		print "BLACKLISTED direct!", sn, s.lastReported
+		s.lastReported[0]=1 
+
+        if s.lastReported[1]==0 and s.upstreamfails>limtime and s.upstream==3:
+                if s.cloud not in clouds:
+                        clouds[s.cloud]=dict()
+                if sn not in clouds[s.cloud]: clouds[s.cloud][sn]=''
+                clouds[s.cloud][sn]+="Failing to redirect upstream from site during more than "+str(cuthours)+" in last "+str(hours)+" hours. "
+                print "BLACKLISTED upstream!", sn, s.lastReported
+                s.lastReported[1]=1
+
+        if s.lastReported[2]==0 and s.downstreamfails>limtime and s.downstream==3:
+                if s.cloud not in clouds:
+                        clouds[s.cloud]=dict()
+                if sn not in clouds[s.cloud]: clouds[s.cloud][sn]=''
+                clouds[s.cloud][sn]+="Failing to access file via downstream redirection during more than "+str(cuthours)+" in last "+str(hours)+" hours. "
+                print "BLACKLISTED downstream!", sn, s.lastReported
+                s.lastReported[2]=1
+print alljson
+
+f1 = open('LastReported.json','w')
+towrite=dict()
+for s in Sites.values():
+	towrite[s.name]=s.lastReported 
+f1.write(json.dumps(towrite))
+f1.close()
+
+f1 = open('InstantMailing.json','w')
+f1.write(json.dumps(alljson))
+f1.close()
+
+#for s in Sites.values():
+#	s.prn()
