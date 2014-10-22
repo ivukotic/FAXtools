f35f6d03cefa99a947cdd79a47d59c3a59f520e1
Ilija Vukotic <ivukotic@cern.ch>
2014-10-22 13:23:24 -0500

    bfix

--- adcmusr3/collectFAXtopology.sh
+++ adcmusr3/collectFAXtopology.sh
@@ -7,8 +7,8 @@ localSetupAGIS
 
 
 cd ~/FAXtools/adcmusr3/
-python collectFAXtopology.py /data/adcmusr3/out
+python collectFAXtopology.py /data/adcmusr3/out/
 
-python collectFAXredirectors.py /data/adcmusr3/out
+python collectFAXredirectors.py /data/adcmusr3/out/
 
-python generateInstantMails.py /data/adcmusr3/out
+python generateInstantMails.py /data/adcmusr3/out/
