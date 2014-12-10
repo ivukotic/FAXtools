33afbcace66ae72706d7b976ccb6e8578b96dab5
Ilija Vukotic <ivukotic@cern.ch>
2014-12-09 18:37:31 -0600

    sec try injector

--- tools/summaryInjector/inject.sh
+++ tools/summaryInjector/inject.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 for i in `seq 1 10`;
     do
-        timestamp=$(date +%s)
-        echo $i $timestamp
-        sed -i 's/ENDTIME/$timestamp/g' msg.xml msgts.xml
-        cat msgts.xml | nc -u atl-prod05.slac.stanford.edu 9931
+        ET=$(date +%s)
+        DT=60
+        ST=`expr $ET - $DT`
+        echo $i $ST $ET
+        sed "s/STARTTIME/$ST/g" msg.xml > .msg1.xml
+        sed "s/ENDTIME/$ET/g" .msg1.xml > .msg2.xml
+        nc -vu 134.79.200.87 9931 < .msg2.xml
+        # nc -vu atl-prod05.slac.stanford.edu 9931 < .msg2.xml
+        sleep $DT
     done  
         
 
