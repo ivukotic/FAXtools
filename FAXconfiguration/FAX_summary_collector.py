b0f5b1520a55b681a0499b3859cfdd7952982db8
Ilija Vukotic <ivukotic@cern.ch>
2015-06-18 14:16:24 +0200

    small stuff

--- FAXconfiguration/FAX_summary_collector.py
+++ FAXconfiguration/FAX_summary_collector.py
@@ -54,8 +54,6 @@ class host:
         print '\tip:',self.ip, 'tos:',self.tos
         print '\tnconn:',self.nconn,'\t avg. conn time:', self.ctime, '\ttimeouts:',self.timeouts,'\terrors:',self.errors,'\tredirects:',self.redirects,'\tdelays:',self.delays
     def postToFlume(self):
-        #v={"timestamp":int(time.time())*1000, "connections":self.nconn,"avg. connnection time":self.ctime, "timeouts":self.timeouts, "errors":self.errors, "redirects":self.redirects, "delays":self.delays }
-        #m = {"headers":{"timestamp":int(time.time())*1000}, "body":str(v)}
         m = {
             "headers":{"type":"FAXredirectors", "timestamp":int(time.time()*1000), "redirector": self.ip, "connections":self.nconn,"ctime":self.ctime, "timeouts":self.timeouts, "errors":self.errors, "redirects":self.redirects, "delays":self.delays },
             "body":''
--- FAXconfiguration/sender.sh
+++ FAXconfiguration/sender.sh
@@ -4,3 +4,28 @@ source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
 source ${ATLAS_LOCAL_ROOT_BASE}/packageSetups/atlasLocalFAXSetup.sh --faxtoolsVersion ${faxtoolsVersionVal}
 voms-proxy-init -valid 24:0 -voms atlas -pwstdin < /home/ivukotic/gridlozinka.txt
 ../FAX_summary_collector.py > sender.log 2>&1
+
+##### flume sink is set like this:
+# faxAgent.sinks.ESFAXredirectorsOut.indexName=fax_summary_redirectors
+# faxAgent.sinks.ESFAXredirectorsOut.indexType=summary_redirectors
+
+#####  ES template is like this
+#   
+#   curl -XPOST 'http://aianalytics01.cern.ch:9200/_template/template_1' -d '{
+#       "template" : "fax_summary_red*",
+#       "settings" : {  "number_of_shards" : 5, "number_of_replicas" : 2 },
+#       "mappings" : {
+#           "summary_redirectors" : {
+#               "_source" : { "enabled" : true },
+#               "properties" : {
+#                   "@fields.redirector" : { "type" : "string", "index" : "not_analyzed" },
+#                   "@fields.connections" : { "type" : "integer", "index" : "analyzed" },
+#                   "@fields.ctime" : { "type" : "integer", "index" : "analyzed" },
+#                   "@fields.delays" : { "type" : "integer", "index" : "analyzed" },
+#                   "@fields.timeouts" : { "type" : "integer", "index" : "analyzed" },
+#                   "@fields.redirects" : { "type" : "integer", "index" : "not_analyzed" },
+#                   "@fields.errors" : { "type" : "integer", "index" : "not_analyzed" }
+#               }
+#           }
+#       }
+#   }'
